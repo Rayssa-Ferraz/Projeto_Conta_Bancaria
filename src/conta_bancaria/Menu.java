ee90f8bbba04281ca6eb0508199ba7be0760dd6c
Rayssa Ferraz <rayssa_nana@hotmail.com>
2025-09-25 17:22:33 -0300

    :sparkles: Construção da Classe ContaController e Atualização do Menu

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,84 +1,25 @@
 package conta_bancaria;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-	import conta_bancaria.model.ContaCorrente;
+import conta_bancaria.controller.ContaController;
+import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.model.ContaPoupanca;
 import conta_bancaria.util.Cores;
 
 
 public class Menu {
-
+	
+	private static final Scanner leia = new Scanner(System.in);
+	private static final ContaController contaController = new ContaController();
+	
 	public static void main(String[] args) {
-		Scanner leia = new Scanner(System.in);
-		
+				
 		int opcao;
 		
-		//Instanciar Objetos da Clasee Conta
+		criarContasTeste();
 		
-		/*Conta c1 = new Conta(1, 123, 1, "Cintia Dourado", 500000.00f);
-		
-		Conta c2 = new Conta(2, 123, 2, "Priscila Lins", 500000.00f);
-
-		System.out.println("O Saldo da conta é: " + c1.getSaldo());
-		
-		c1.setSaldo(600000.00f);
-		
-		System.out.println("O Saldo da conta é: " + c1.getSaldo());
-		
-		c1.visualizar();
-		
-		c2.visualizar();
-		
-		System.out.println(c1.sacar(1000));
-		System.out.println("O Saldo da conta é: " + c1.getSaldo());
-
-		System.out.println(c2.sacar(1000000));
-		System.out.println("O Saldo da conta é: " + c2.getSaldo());
-		
-		c1.depositar(5000);
-		System.out.println("O Saldo da conta é: " + c1.getSaldo());*/
-		
-		// Instanciar Objetos da Classe ContaCorrente
-		
-				System.out.println("\n\nInstanciar Objeto da Classe ContaCorrente");
-				
-				ContaCorrente cc1 = new ContaCorrente(3, 456, 1, "Thuany Silva", 1000000.00f, 100000.00f);
-				
-				cc1.visualizar();
-				
-				// Sacar ContaCorrente
-				System.out.println("Sacar R$ 2.000.000.00f: " + cc1.sacar(2000000.00f));
-				System.out.println("O Saldo da conta é: R$ " + cc1.getSaldo());
-				
-				System.out.println("Sacar R$ 2.000.00f: " + cc1.sacar(2000.00f));
-				System.out.println("O Saldo da conta é: R$ " + cc1.getSaldo());
-				
-				// Depositar ContaCorrente
-				System.out.println("Depositar R$ 5.000.00");
-				cc1.depositar(5000.00f);
-				System.out.println("O Saldo da conta é: R$ " + cc1.getSaldo());
-				
-				// Instanciar Objeto da Classe ContaPopupanca
-				
-				System.out.println("\n\nInstanciar Objeto da Classe ContaPoupnca");
-				
-				ContaPoupanca cp1 = new ContaPoupanca(4, 456, 2, "Maria da Silva", 100000.00f, 15);
-						
-				cp1.visualizar();
-						
-				// Sacar ContaPoupanca
-				System.out.println("Sacar R$ 200.000.00: " + cp1.sacar(200000.00f));
-				System.out.println("O Saldo da conta é: R$ " + cp1.getSaldo());
-						
-				System.out.println("Sacar R$ 2.000.00: " + cp1.sacar(2000.00f));
-				System.out.println("O Saldo da conta é: R$ " + cp1.getSaldo());
-						
-				// Depositar ContaPopupanca
-				System.out.println("Depositar R$ 5.000.00");
-				cp1.depositar(5000.00f);
-				System.out.println("O Saldo da conta é: R$ " + cp1.getSaldo());
-
 		while (true) {
 
 			System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "*****************************************************");
@@ -101,9 +42,15 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
 			
-
+		try {
 			opcao = leia.nextInt();
+			leia.nextLine();
+		}catch(InputMismatchException e) {	
+			opcao = -1;
+			System.out.println("\nDigite um número inteiro entre 0 e 8");
+			leia.nextLine();
 			
+		}
 			if(opcao == 0) {
 				System.out.println(Cores.TEXT_CYAN_BOLD_BRIGHT + "\nLumina Bank - Onde seu dinheiro brilha mais!");
 				
@@ -117,38 +64,49 @@ public class Menu {
 				switch (opcao) {
 				case 1:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Criar Conta\n\n");
-
+					
+					keyPress();
 					break;
 				case 2:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Listar todas as Contas\n\n");
-
+					
+					listarContas();
+					
+					keyPress();
 					break;
 				case 3:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Consultar dados da Conta - por número\n\n");
-
+					
+					keyPress();
 					break;
 				case 4:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Atualizar dados da Conta\n\n");
-
+					
+					keyPress();
 					break;
 				case 5:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Apagar a Conta\n\n");
-
+					
+					keyPress();
 					break;
 				case 6:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Saque\n\n");
-
+					
+					keyPress();
 					break;
 				case 7:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Depósito\n\n");
-
+					
+					keyPress();
 					break;
 				case 8:
 					System.out.println(Cores.TEXT_CYAN_BRIGHT + "Transferência entre Contas\n\n");
-
+					
+					keyPress();
 					break;
 				default:
 					System.out.println(Cores.TEXT_RED_BOLD_BRIGHT + "\nOpção Inválida!\n");
+					keyPress();
 					break;
 			}
 		}
@@ -162,6 +120,20 @@ public class Menu {
 		System.out.println(Cores.TEXT_CYAN_BRIGHT + "Rayssa Ferraz - rayssa_nana@hotmail.com               ");
 		System.out.println(Cores.TEXT_CYAN_BRIGHT + "github.com/Rayssa-Ferraz                              ");
 		System.out.println(Cores.TEXT_CYAN_BRIGHT + "******************************************************");
+	}
+	
+	public static void keyPress() {
+		System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para continuar...");
+		leia.nextLine();
+	}
+	
+	private static void criarContasTeste() {
+		contaController.cadastrar(new ContaCorrente(1, 456, 1, "Thuany Silva", 1000000.00f, 100000.00f));
+		contaController.cadastrar(new ContaCorrente(1, 456, 2, "MArcia Condarco", 1000000.00f, 10));
+	}
+	
+	private static void listarContas() {
+		contaController.listarTodas();
 	}
 }
 			
